ea82786357cdbcc0a4c5fe4c573ea83be573ade7
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-08-15 20:26:11 +0530

    welcome to java problem code

---
+++ Welcometojava.java
@@ -0,0 +1,8 @@
+public class Welcometojava {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
